436ed648787d667607b3e8fa3ff6bd4efcf80873
Miguel neto <djmiguelbit@gmail.com>
2022-04-06 20:23:35 -0300

    Alterando Comentarios Liferay

--- projetos/Liferay.py
+++ projetos/Liferay.py
@@ -1,27 +1,30 @@
-
+#atribuindo valores da entrada 01
 livro = float(12.49)
 cd = float(14.99)
 barraChorolate = float(0.85)
 
+##Logica da saida 01
 entradaA = float((cd * 10) / 100)
 somaA = float(cd + entradaA)
 totalA = float(somaA + barraChorolate + livro)
 
+#atribuindo valores da entrada 02
 caixaChocolateImportado = float(10.00)
 frascoPerfumeImportado = float(47.50)
 
+#Logica da saida 02
 entradaB = float((caixaChocolateImportado * 5) / 100)
 entradaBB = float((frascoPerfumeImportado * 15) / 100)
 somaB = float((entradaB + entradaBB))
 totalB = float(caixaChocolateImportado + entradaB + frascoPerfumeImportado + entradaBB)
 
-
+#atribuindo valores da entrada 03
 frascoPerfumeImportadoC = float(27.99)
 frascoPerfume = float(18.99)
 cartelaAnalgesico = float(9.75)
 caixaChocolateImportadoC = float(11.25)
 
-
+#Logica da saida 03
 entradaC = float((frascoPerfumeImportadoC * 15) / 100)
 somaC = float(frascoPerfumeImportadoC + entradaC)
 entradaCC = float((frascoPerfume * 10) / 100)
@@ -32,7 +35,7 @@ totalC = float(entradaC + entradaCC + entradaCCC)
 valorC = float(somaC + somaCC + somaCCC + cartelaAnalgesico)
 
 
-
+#Resultados saida 1
 print('=========================================================')
 print('Saída 1')
 print('1 Livro: ${:.2f}'.format(livro))
@@ -42,7 +45,7 @@ print('imposto de vendas ${:.2f}'.format(entradaA))
 print('Total: ${:.2f}'.format(totalA))
 
 
-
+#Resultados saida 2
 print('=========================================================')
 print('Saída 2')
 print('1 caixa de chocolates importado: ${:.2f}'.format(caixaChocolateImportado + entradaB))
@@ -51,7 +54,7 @@ print('imposto sobre vendas: ${:.2f}'.format(somaB))
 print('Total: ${:.2f}'.format(totalB))
 
 
-
+#Resultados saida 3
 print('=========================================================')
 print('Saída 3')
 print('1 frasco de perfume importado: ${:.2f}'.format(somaC))
